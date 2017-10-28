2cb180939c4d096c599c7095f88dad456023272f
nurratna <nurratnasarii@gmail.com>
2017-10-28 23:23:20 +0700

    re-config rspec and create Food model spec

---
+++ spec/models/food_model_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe Food do
+  it "is valid with a name and description"
+  it "is invalid without a name"
+  it "is invalid without a description"
+  it "is invalid with a duplicate name"
+end
