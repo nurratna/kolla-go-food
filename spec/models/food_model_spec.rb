eb06c39f06a2602cb37ee3b9a56e6bf0f057caaa
nurratna <nurratnasarii@gmail.com>
2017-10-29 06:21:05 +0700

    describe Food to Model Spec

--- spec/models/food_model_spec.rb
+++ spec/models/food_model_spec.rb
@@ -46,4 +46,48 @@ describe Food do
     food2.valid?
     expect(food2.errors[:name]).to include("has already been taken")
   end
+
+  it "returns a sorted array of results that match" do
+    food1 = Food.create(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 10000.0
+    )
+
+    food2 = Food.create(
+      name: "Kerak Telor",
+      description: "Betawi traditional spicy omelette made from glutinous rice cooked with egg and served with serundeng.",
+      price: 8000.0
+    )
+
+    food3 = Food.create(
+      name: "Nasi Semur Jengkol",
+      description: "Based on dongfruit, this menu promises a unique and delicious taste with a small hint of bitterness.",
+      price: 8000.0
+    )
+
+    expect(Food.by_letter("N")).to eq([food3, food1])
+  end
+
+  it "omits results that do not match" do
+    food1 = Food.create(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 10000.0
+    )
+
+    food2 = Food.create(
+      name: "Kerak Telor",
+      description: "Betawi traditional spicy omelette made from glutinous rice cooked with egg and served with serundeng.",
+      price: 8000.0
+    )
+
+    food3 = Food.create(
+      name: "Nasi Semur Jengkol",
+      description: "Based on dongfruit, this menu promises a unique and delicious taste with a small hint of bitterness.",
+      price: 8000.0
+    )
+
+    expect(Food.by_letter("N")).not_to include(food2)
+  end
 end
