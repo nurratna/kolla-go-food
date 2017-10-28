8b2caeea0a1dd88a5d24407c3fed5758974f0a7b
nurratna <nurratnasarii@gmail.com>
2017-10-28 16:43:34 +0700

    scaffolding the Food and modifying CSS-HTML

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Food.delete_all
+
+Food.create!(
+  name: 'Mie Aceh',
+  description:
+    %{<p>
+      Mie Aceh ini adalah salah satu kuliner primadona dari Aceh.
+      Kepopuleran mie Aceh ini bahkan sudah sampai di kancah Internasional lho!
+      Mie Aceh ini memang punya ciri khas dan cita rasa yang unik, enak dan tiada duanya.
+      Hal ini dikarenakan bumbu yang digunakan untuk memasak ini penuh dengan rempah-rempah.
+    </p>},
+  image_url: 'mie_aceh.jpeg',
+  price: 30000.0
+)
+
+Food.create!(
+  name: 'Ayam Tangkap',
+  description:
+    %{<p>
+      Ayam tangkap ini merupakan hidangan khas spesial dari daerah Aceh Besar.
+      Ayam Tangkap adalah masakan dengan bahan utama ayam yang telah dipotong-potong
+      lalu dimasak bersama cabe hijau, daun pandan, dan daun kari/daun teumuru.
+    </p>},
+  image_url: 'ayam_tangkap.jpeg',
+  price: 35000.0
+)
+
+Food.create!(
+  name: 'Sate Matang',
+  description:
+    %{<p>
+      Sate ini dinamakan sate matang karena berasal dari daerah Matang di Kabupaten Bireuen Aceh.
+      Sate Matang ini tidak seperti sate pada umumnya yang dibakar
+      lalu diberi bumbu kecap dan saus kacang, lho.
+      Sate matang justru disajikan dengan cara disiram dengan kuah soto yang gurih.
+    </p>},
+  image_url: 'sate_matang.jpeg',
+  price: 20000.0
+)
