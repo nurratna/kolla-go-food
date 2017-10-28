8cfb4c9493500aa115f8a7a07ab35d5748174a4b
nurratna <nurratnasarii@gmail.com>
2017-10-28 22:08:57 +0700

    validating model Buyer

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,8 @@
 class Buyer < ApplicationRecord
+  validates :email, :phone, :address, presence: true
+  validates :email, uniqueness: true, format: {
+		with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
+		message: "Email format not valid"
+	}
+  validates :phone, numericality: true, length: { in: 10..12 }
 end
