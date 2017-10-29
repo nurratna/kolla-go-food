9ea63c27f977e539a3181b6728b02c56b157974c
nurratna <nurratnasarii@gmail.com>
2017-10-29 07:22:37 +0700

    adding specs with factory girl - simplying syntax

--- spec/models/food_model_spec.rb
+++ spec/models/food_model_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 
 describe Food do
   it "has a valid factory" do
-    expect(FactoryGirl.build(:food)).to be_valid
+    expect(build(:food)).to be_valid
   end
 
   it "is valid with a name and description" do
-    expect(FactoryGirl.build(:food)).to be_valid
+    expect(build(:food)).to be_valid
   end
 
   it "is invalid without a name" do
-    food = FactoryGirl.build(:food, name: nil)
+    food = build(:food, name: nil)
     food.valid?
     expect(food.errors[:name]).to include("can't be blank")
   end
 
   it "is invalid without a description" do
-    food = FactoryGirl.build(:food, description: nil)
+    food = build(:food, description: nil)
     food.valid?
     expect(food.errors[:description]).to include("can't be blank")
   end
 
   it "is invalid with a duplicate name" do
-    food1 = FactoryGirl.create(:food, name: "Nasi Uduk")
-    food2 = FactoryGirl.build(:food, name: "Nasi Uduk")
+    food1 = create(:food, name: "Nasi Uduk")
+    food2 = build(:food, name: "Nasi Uduk")
 
     food2.valid?
     expect(food2.errors[:name]).to include("has already been taken")
