a00d6ef79f38f6e42889b65d6e2401d42a2de210
nurratna <nurratnasarii@gmail.com>
2017-10-30 06:04:26 +0700

    Adding Food to Cart - Model spec, Factory, Migration for LineItem

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,7 @@
 class Food < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :name, :description, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :name, uniqueness: true
@@ -10,4 +13,12 @@ class Food < ApplicationRecord
   def self.by_letter(letter)
     where('name LIKE ?', "#{letter}%").order(:name)
   end
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
