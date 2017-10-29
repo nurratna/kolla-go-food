4aebaba80ee58909566abcb85ca8dfa23d218115
nurratna <nurratnasarii@gmail.com>
2017-10-29 07:26:02 +0700

    adding specs with factory girl use Faker

--- spec/factories/food.rb
+++ spec/factories/food.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :food do
-    name "Nasi Uduk"
-    description "Betawi style steamed rice cooked in coconut milk. Delicious!"
+    name { Faker::Food.dish }
+    description { Faker::Food.ingredient }
     price 10000.0
   end
 end
