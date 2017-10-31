882d49c47fc7cc77b52ab0b5d8059531c9debe5b
nurratna <nurratnasarii@gmail.com>
2017-11-01 06:27:42 +0700

    refactor foods spec

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -1,13 +1,14 @@
 FactoryGirl.define do
-  factory :food do
-    name { Faker::Food.dish }
-    description { Faker::Food.ingredient }
-    price 10000.0
-  end
+	factory :food do
+		name { Faker::Food.dish }
+		description { Faker::Food.ingredient }
+		price 10000.0
+		image_url 'image.png'
+	end
 
-  factory :invalid_food, parent: :food do
-    name nil
-    description nil
-    price 1000.0
-  end
+	factory :invalid_food, parent: :food do
+		name nil
+		description nil
+		price 10000.0
+	end
 end
