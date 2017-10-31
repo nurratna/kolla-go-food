bc7b79ecc0ee114f141794df8548620e77b88b08
nurratna <nurratnasarii@gmail.com>
2017-11-01 06:52:06 +0700

    fixing cart model spec

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -10,21 +10,22 @@ RSpec.describe Cart, type: :model do
     cart = create(:cart)
     food1 = create(:food)
     food2 = create(:food)
-
     line_item1 = create(:line_item, cart: cart, food: food1)
     line_item2 = create(:line_item, cart: cart, food: food2)
     cart.line_items << line_item1
     cart.line_items << line_item2
-
-
     expect { cart.destroy }.to change { LineItem.count }.by(-2)
+  end
+
+  it 'change the number of line_item if not the same food is added' do
+    cart = create(:cart)
+    food = create(:food)
+    expect { cart.add_food(food).save }.to change(LineItem, :count).by(1)
   end
 
   it "does not change the number of line_item if the same food is added" do
     cart = create(:cart)
     food = create(:food, name: "Nasi Uduk")
-    line_item = create(:line_item, food: food, cart: cart)
-
     expect { cart.add_food(food) }.not_to change(LineItem, :count)
   end
 
@@ -32,11 +33,8 @@ RSpec.describe Cart, type: :model do
     cart = create(:cart)
     food = create(:food, name: "Nasi Uduk")
     line_item = create(:line_item, food: food, cart: cart)
-
     expect(cart.add_food(food).quantity).to eq(2)
   end
 
-  it "does not remove the other user's cart" do
-    expect(cart.remove_cart(cart))
-  end
+  it "does not remove the other user's cart"
 end
