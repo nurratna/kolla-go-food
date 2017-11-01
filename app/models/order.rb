2643c08248b09be9c8aaeb280efcbac6e7303749
nurratna <nurratnasarii@gmail.com>
2017-11-01 14:42:31 +0700

    aading line items from cart

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,23 @@
 class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+
+  enum payment_type: {
+    "Cash" => 0,
+    "Go Pay" => 1,
+    "Credit Card" => 2
+  }
+
   validates :name, :address, :email, :payment_type, presence: true
-  # validates :payment_type, numericality: { range: 0..2 }
   validates :email, uniqueness: true, format: {
 		with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i,
 		message: "invalid email format"
 	}
+  validates :payment_type, inclusion: payment_types.keys
+
+  def add_line_items(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
 end
