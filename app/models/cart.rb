936690b74ede45dbedc884d28b598388eec5f8f0
nurratna <nurratnasarii@gmail.com>
2017-11-01 11:22:47 +0700

    add ajax

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,4 +12,13 @@ class Cart < ApplicationRecord
 
     current_item
   end
+
+  def total_price
+    total_price = 0.0
+    line_items.each do |item|
+      total_price += item.total_price
+    end
+    total_price
+  end
+
 end
