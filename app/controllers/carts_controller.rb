7033366dffc715396639b205c4a9dce791d3b42a
nurratna <nurratnasarii@gmail.com>
2017-11-01 09:42:45 +0700

    modifying method destroy

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -54,16 +54,11 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    if session[:cart_id] == @cart.id
-      @cart.destroy
-      # session[:cart_id] = 
-      notice = 'Cart was successfully destroyed.'
-    else
-      notice = ''
-    end
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] == nil
 
     respond_to do |format|
-      format.html { redirect_to store_index_path, notice: notice }
+      format.html { redirect_to store_index_path, notice: 'Cart was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
