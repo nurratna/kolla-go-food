6a5109117fc7d566c9f689d89d32a4a026419402
nurratna <nurratnasarii@gmail.com>
2017-11-01 07:03:34 +0700

    adding method total_price inspec and model line_item

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -4,4 +4,12 @@ describe LineItem do
   it 'has a valid factory' do
     expect(build(:line_item)).to be_valid
   end
+
+  it 'can calculate total_price' do
+    cart = create(:cart)
+    food = create(:food, price: 10000.0)
+    line_item = create(:line_item, quantity: 3, food: food, cart: cart)
+
+    expect(line_item.total_price).to eq(30000.0)
+  end
 end
