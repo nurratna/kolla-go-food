1eb10261da877eb82e3bbea995a1674f10973d51
nurratna <nurratnasarii@gmail.com>
2017-11-08 10:18:36 +0700

    modifying food

--- app/models/food.rb
+++ app/models/food.rb
@@ -6,7 +6,7 @@ class Food < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :name, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif|jpg|jpeg|png)\Z}i,
+    with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image'
   }
 
