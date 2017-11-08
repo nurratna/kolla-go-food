1cb94e502fe65697b3cd315fba1b462eac7e44ae
nurratna <nurratnasarii@gmail.com>
2017-11-08 15:01:09 +0700

    adding feature tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108054341) do
+ActiveRecord::Schema.define(version: 20171108040309) do
 
   create_table "buyers", force: :cascade do |t|
     t.string "email"
